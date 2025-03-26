f8c393472c730803bb99342aa4eb06c2d21dd2e2
Zakriya0344699 <0344699@student.rocvantwente.nl>
2025-03-26 11:21:22 +0100

    opdracht 1

---
+++ c#/c#/Models/ClassExtensions.cs
@@ -0,0 +1,32 @@
+﻿namespace csharp.Models
+{
+    public static class StringExtensions
+    {
+        /// <param name="Palindrome"></param>
+
+        public static bool IsPalindrome(this string Palindrome)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(Palindrome)) return false;
+
+                string LowerPalindrome = Palindrome.ToLowerInvariant();
+                var Reversed = ReverseString(LowerPalindrome);
+
+                return Reversed == LowerPalindrome;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Niks ingevoerd: {ex.Message}");
+                return false;
+            }
+        }
+        /// <param name="input"></param>
+        private static string ReverseString(string input)
+        {
+            var Chars = input.ToCharArray();
+            Array.Reverse(Chars);
+            return new string(Chars);
+        }
+    }
+}
